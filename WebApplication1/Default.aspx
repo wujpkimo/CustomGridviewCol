ac6d10ae1dd87d5773bfe9001a502de21d073450
Jason <wujpkimo@gmail.com>
2020-05-14 15:20:44 +0800

    加入專案檔案。

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>ASP.NET</h1>
+        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
+        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <h2>CustomGridView</h2>
+
+            <asp:GridView ID="GdvTest" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="ProductName" HeaderText="Product Name" />
+                    <asp:BoundField DataField="SupplierName" HeaderText="Supplier Name" />
+                    <asp:BoundField DataField="CategoryName" HeaderText="Category Name" />
+                    <asp:BoundField DataField="QuantityPerUnit" HeaderText="Qty/Unit" />
+                    <asp:BoundField DataField="CreateDate" DataFormatString="{0:yyyy/MM/dd}" HeaderText="Create Date"
+                        HtmlEncode="False" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var fateData = new List<Test>()
+            {
+                new Test("TestProductName","TestCategoryName","TestSupplierName",10,DateTime.Now),
+                new Test("TestProductName1","TestCategoryName1","TestSupplierName1",5,DateTime.Now),
+                new Test("TestProductName2","TestCategoryName2","TestSupplierName2",10,DateTime.Now),
+                new Test("TestProductName3","TestCategoryName3","TestSupplierName3",10,DateTime.Now),
+                new Test("TestProductName4","TestCategoryName4","TestSupplierName4",10,DateTime.Now),
+            };
+            GdvTest.DataSource = fateData;
+            GdvTest.DataBind();
+        }
+
+        private class Test
+        {
+            public string ProductName { get; set; }
+            public string CategoryName { get; set; }
+            public string SupplierName { get; set; }
+            public int QuantityPerUnit { get; set; }
+            public DateTime CreateDate { get; set; }
+
+            public Test(string productName, string categoryName, string supplierNmae, int qty, DateTime createDate)
+            {
+                this.ProductName = productName;
+                this.CategoryName = categoryName;
+                this.SupplierName = supplierNmae;
+                this.QuantityPerUnit = qty;
+                this.CreateDate = createDate;
+            }
+        }
+    }
+}
